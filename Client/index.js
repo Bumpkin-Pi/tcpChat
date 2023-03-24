12df3704d862f43bef5f37dca98daa53655631a2
BumpkinPi <h.person.notrobot@gmail.com>
2023-03-24 22:13:55 +0000

    Initial commit

---
+++ Client/index.js
@@ -0,0 +1,95 @@
+const Net = require('net');
+const chalk = require("chalk");
+const blessed = require('blessed');
+let screen = blessed.screen();
+let body = blessed.box({
+    top: 0,
+    left: 0,
+    height: '100%-1',
+    width: '100%',
+    keys: true,
+    mouse: true,
+    alwaysScroll: true,
+    scrollable: true,
+    scrollbar: {
+        ch: ' ',
+        bg: 'red'
+    }
+});
+let inputBar = blessed.textbox({
+    bottom: 0,
+    left: 0,
+    height: 1,
+    width: '100%',
+    keys: true,
+    mouse: true,
+    inputOnFocus: true,
+    style: {
+        fg: 'black',
+        bg: 'blue'  // Blue background so you see this is different from body
+    }
+});
+
+
+
+
+let name = process.argv[2] || "User";
+console.log(name)
+
+screen.append(body);
+screen.append(inputBar);
+screen.key(['escape', 'q', 'C-c'], (ch, key) => (process.exit(0)));
+
+inputBar.on('submit', (text) => {
+    log(chalk.red("You: ")+chalk.white(text));
+    client.write(text);
+    inputBar.clearValue();
+    inputBar.focus();
+});
+function log(text) {
+    body.pushLine(text);
+    screen.render();
+}
+screen.key('enter', (ch, key) => {
+    inputBar.focus();
+});
+
+
+// The port number and hostname of the server.
+const port = 25567;
+const host = '127.0.0.1';
+const timeout = 10;
+
+// Create a new TCP client.
+const client = new Net.Socket();
+
+// Wait for 2 seconds before attempting to connect to the server.
+setTimeout(function() {
+    // Send a connection request to the server.
+    client.connect({ port: port, host: host }, function() {
+        // If there is no error, the server has accepted the request and created a new
+        // socket dedicated to us.
+        log('TCP connection established with the server.\n\n');
+        client.write(`/name ${name}`);
+
+        // The client can now send data to the server by writing to its socket.
+        // client.write('Hello, server.');
+    });
+}, timeout);
+
+// The client can also receive data from the server by reading from its socket.
+client.on('data', function(chunk) {
+
+
+
+    log(chunk.toString());
+
+    // Request an end to the connection after the data has been received.
+    // client.end();
+});
+
+client.on('end', function() {
+    log('Requested an end to the TCP connection');
+    process.exit();
+});
+
---
+++ Server/index.js
@@ -0,0 +1,62 @@
+const HOST = '127.0.0.1';
+const PORT = 25567;
+const connectedSockets = new Set();
+
+const net = require("net");
+const chalk = require("chalk");
+const fs = require('fs');
+let history = fs.readFileSync('history.txt').toString().split("\n");
+
+
+connectedSockets.broadcast = function(data, except) {
+    for (let sock of this) {
+        if (sock !== except) {
+            sock.write(data);
+        }
+    }
+}
+
+const server = net.createServer().listen(PORT, HOST);
+server.on('connection', function(sock){
+    sock.id = `${sock.remoteAddress}:${sock.remotePort}`;
+    connectedSockets.add(sock);
+    console.log(`Client ${sock.id} connected`);
+    for (message in history){
+        sock.write(history[message]+"\n");
+    }
+
+
+    connectedSockets.broadcast(`New client ${sock.id} connected.\n`, sock);
+
+
+
+
+
+    sock.on('data', function(data) {
+        if (data.toString()[0] === "/"){
+            if (data.toString().split(" ")[0] === "/name"){
+                name = (data.toString().replace("/name ", "").replaceAll(" ", "_"))
+                // history.push(chalk.yellow(`User "${sock.id}" changed name to "${name}"`))
+                // console.log(chalk.yellow(`User "${sock.id}" changed name to "${name}"`));
+                // fs.appendFileSync("history.txt", (`User "${sock.id}" changed name to "${name}"`));
+                // connectedSockets.broadcast(chalk.yellow(`User "${sock.id}" changed name to "${name}"`))
+
+                sock.id = name;
+            }
+        }else{
+            // console.log(`Received from ${sock.id}: ${data.toString()}`);
+            history.push(`${chalk.green(sock.id)}: ${(data.toString())}`)
+            console.log(`${chalk.green(sock.id)}: ${(data.toString())}`);
+            fs.appendFileSync("history.txt", `${(sock.id)}: ${(data.toString())}\n`);
+            connectedSockets.broadcast(`${chalk.green(sock.id)}: ${(data.toString())}`, sock);
+        }
+    });
+
+    sock.on('end', function() {
+        console.log(`Client ${sock.id} disconnected`);
+        connectedSockets.delete(sock);
+        connectedSockets.broadcast(chalk.yellow(`User ${chalk.green(sock.id)} disconnected`), sock);
+    });
+});
+
+console.log(`Server listening on ${HOST}:${PORT}`);
